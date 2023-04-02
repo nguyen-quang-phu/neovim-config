ad866b0d309cd5da3582ddfc8c3b9ea4faac1ae0
Harvey <harvey@Harveys-Macbook.local>
2023-04-02 20:14:40 +0700

    feat: setup auto pair for javascript and not for ruby

--- plugins.lua
+++ plugins.lua
@@ -190,6 +190,39 @@ local plugins = {
     opts = {},
     lazy = false,
   },
+  {
+    "windwp/nvim-autopairs",
+    opts = {
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function(_, opts)
+      require("nvim-autopairs").setup(opts)
+
+      -- setup cmp for autopairs
+      local cmp = require "cmp"
+      local handlers = require "nvim-autopairs.completion.handlers"
+      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+
+      require("cmp").event:on(
+        "confirm_done",
+        cmp_autopairs.on_confirm_done {
+          filetypes = {
+            ["*"] = {
+              ["("] = {
+                kind = {
+                  cmp.lsp.CompletionItemKind.Function,
+                  cmp.lsp.CompletionItemKind.Method,
+                },
+                handler = handlers["*"],
+              },
+            },
+            ruby = false,
+          },
+        }
+      )
+    end,
+  },
 }
 
 return plugins
